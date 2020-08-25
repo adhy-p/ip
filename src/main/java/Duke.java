e9e0186103a8df77d7da8d7d0d62a69fab3b42de
adhy-p <adhyp02@gmail.com>
2020-08-25 22:46:55 +0800

    Add greet method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
 public class Duke {
+    public static void greet(){
+        // simply greet the user and exit
+        String lines = "-----------------------------------";
+        System.out.println(lines);
+        System.out.println("Hello, I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(lines);
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println(lines);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +16,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
